9c8f4b25bd11b28ee9a8ddc3a4e66592c582bfca
Nilesh Ruby Rails dummy projects <nilesh.mistry79@gmail.com>
2016-10-15 14:02:06 +0000

    update views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonyoums"
+  end  
+  
 end
